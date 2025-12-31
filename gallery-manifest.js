d97e2982a2279f350d8927c3c3ada1105aef1933
karen marie <153211090+kasigila@users.noreply.github.com>
2025-12-31 13:36:41 -0500

    Create gallery-manifest.js

---
+++ gallery-manifest.js
@@ -0,0 +1,60 @@
+// gallery-manifest.js
+// Boards (albums) + filenames
+// Path format: `${ROOTS[type]}${board}/${filename}`
+
+window.BYZ_GALLERY = {
+  ROOTS: {
+    groove: "groove-memory-gallery/",
+    tempo: "tempo-memory-gallery/",
+  },
+
+  BOARDS: {
+    groove: {
+      "december-26-2025": [
+        "FYRM0007.jpg","FYRM0021.jpg","FYRM0056.jpg","FYRM0071.jpg","FYRM0079.jpg",
+        "FYRM0084.jpg","FYRM0091.jpg","FYRM0135.jpg","FYRM0174.jpg","FYRM0240.jpg",
+        "FYRM0253.jpg","FYRM0292.jpg","FYRM9580.jpg","FYRM9599.jpg","FYRM9622.jpg",
+        "FYRM9658.jpg","FYRM9683.jpg","FYRM9686.jpg","FYRM9693.jpg","FYRM9717.jpg",
+        "FYRM9772.jpg","FYRM9855.jpg","FYRM9909.jpg","FYRM9957.jpg","FYRM9994.jpg"
+      ],
+      "december-6-2025": [
+        "FYRM8087.jpg","FYRM8089.jpg","FYRM8095.jpg","FYRM8101.jpg","FYRM8107.jpg",
+        "FYRM8140.jpg","FYRM8143.jpg","FYRM8182.jpg","FYRM8211.jpg","FYRM8230.jpg",
+        "FYRM8266.jpg","FYRM8320.jpg","FYRM8356.jpg","FYRM8366.jpg","FYRM8395.jpg",
+        "FYRM8413.jpg","FYRM8466.jpg","FYRM8506.jpg"
+      ],
+      "may-24-2025": [
+        "692A0026.jpg","692A0037.jpg","692A0048.jpg","692A0461.jpg","692A0512.jpg",
+        "692A0560.jpg","692A9142.jpg","692A9161.jpg","692A9194.jpg","692A9209.jpg",
+        "692A9272.jpg","692A9372.jpg","692A9920.jpg","692A9923.jpg","692A9958.jpg",
+        "692A9971.jpg","692A9996.jpg","FYRM1284.jpg","FYRM1318.jpg"
+      ]
+    },
+
+    tempo: {
+      "april-4-2025": [
+        "692A0582.jpg","692A0817.jpg","692A1059.jpg"
+      ],
+      "july-11-2025": [
+        "FYRM0874.jpg","FYRM0884.jpg","FYRM0903.jpg","FYRM0940.jpg","FYRM0998.jpg",
+        "FYRM1023.jpg","FYRM1051.jpg","FYRM1054.jpg","FYRM1131.jpg","FYRM1153.jpg",
+        "FYRM1221.jpg","FYRM1227.jpg"
+      ],
+      "june-27-2025": [
+        "692A9382.jpg","692A9391.jpg","692A9396.jpg","692A9425.jpg","692A9454.jpg",
+        "692A9494.jpg","692A9508.jpg","692A9522.jpg","692A9582.jpg","692A9614.jpg",
+        "692A9622.jpg","692A9632.jpg","692A9660.jpg","692A9691.jpg","692A9755.jpg",
+        "692A9781.jpg","692A9805.jpg","692A9836.jpg","692A9843.jpg","692A9856.jpg"
+      ],
+      "may-16-2025": [
+        "692A9492.jpg","692A9516.jpg","692A9523.jpg","692A9570.jpg","692A9577.jpg",
+        "692A9591.jpg","692A9592.jpg","692A9622.jpg","692A9627.jpg","692A9642.jpg",
+        "692A9663.jpg","692A9677.jpg","692A9682.jpg","692A9690.jpg","692A9698.jpg"
+      ],
+      "may-30-2025": [
+        "692A0187.jpg","692A0212.jpg","692A0236.jpg","692A0266.jpg","692A0308.jpg",
+        "692A0322.jpg","692A0369.jpg","692A0383.jpg"
+      ]
+    }
+  }
+};
